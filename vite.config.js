03b7f244c8696bae69122cd8371a74c8729969c9
jelly <jellypnt1311@gmail.com>
2025-01-05 21:43:14 +0700

    fix: update homepage URL in package.json and set base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/scupos-fe/',
   build: {
     target: 'esnext' //browsers can handle the latest ES features
   },
