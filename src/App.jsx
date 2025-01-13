c9ea48a1bb58a7b429531cf5a25564acc9c7e4ba
jelly <jellypnt1311@gmail.com>
2025-01-13 23:00:52 +0700

    refactor: update data fetching logic to use environment variables and enhance search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,68 @@
-import { useState } from "react";
-import JSZip from "jszip";
+import { useEffect, useState } from "react";
 
-// load the data from the zipped json file
-const data = await fetchAndParseJsonFromZip("data_2023_with_affiliation.zip");
-console.log(data);
+async function fetchData(
+  query = "",
+  index = "articles",
+  page = 1,
+  attributes = ["*"],
+  limit = 10
+) {
+  const url = import.meta.env.VITE_MEILI_URL;
+  const key = import.meta.env.VITE_MEILI_CLIENT_KEY;
 
-async function fetchAndParseJsonFromZip(filePath) {
-  const dataRaw = await fetch(filePath);
-  const zipBlob = await dataRaw.blob();
-  const zip = new JSZip();
-  const zipContent = await zip.loadAsync(zipBlob);
-  const jsonFileName = Object.keys(zipContent.files).find((fileName) =>
-    fileName.endsWith(".json")
-  );
+  console.log(">>> url", url);
 
-  if (!jsonFileName) {
-    throw new Error("No JSON file found in the zip.");
+  const response = await fetch(`${url}/indexes/${index}/search`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${key}`,
+    },
+    body: JSON.stringify({
+      q: query,
+      page,
+      attributesToSearchOn: attributes,
+      hitsPerPage: limit,
+    }),
+  });
+
+  if (!response.ok) {
+    console.log(">>> response", response);
+    throw new Error("Failed to fetch data");
   }
 
-  // Extract and parse the JSON file
-  const jsonFileContent = await zipContent.files[jsonFileName].async("string");
-  const data = JSON.parse(jsonFileContent);
-  return data;
+  return response.json();
 }
+
+const SEARCH_FIELDS_MAPPING = {
+  Title: "dc:title",
+  Author: "indexAuthors",
+  Affiliation: "indexAffiliation",
+  Journal: "prism:publicationName",
+};
+
+const SEARCH_FIELDS_MAPPING_REVERSE = Object.fromEntries(
+  Object.entries(SEARCH_FIELDS_MAPPING).map(([key, value]) => [value, key])
+);
 
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
+  const [searchField, setSearchField] = useState("dc:title");
   const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 10;
-  const renderData = data;
+  const [renderData, setRenderData] = useState(null);
+
+  useEffect(() => {
+    const delayFetch = setTimeout(() => {
+      fetchData(searchTerm, "articles", currentPage, [searchField], 10).then(
+        (data) => {
+          console.log(">>> renderData", data.hits); // Log the correct data
+          setRenderData(data.hits); // Set the fetched data
+        }
+      );
+    }, 500); // Delay of 500ms
+
+    return () => clearTimeout(delayFetch); // Cleanup timeout on dependency change
+  }, [searchTerm, currentPage, searchField]);
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
@@ -39,133 +73,100 @@ function App() {
     setCurrentPage(newPage);
   };
 
-  const filteredData = renderData.filter((item) =>
-    item["dc:title"].toLowerCase().includes(searchTerm.toLowerCase())
-  );
-
-  const paginatedData = filteredData.slice(
-    (currentPage - 1) * itemsPerPage,
-    currentPage * itemsPerPage
-  );
-
-  const totalPages = Math.ceil(filteredData.length / itemsPerPage);
+  const handleSearchFieldChange = (event) => {
+    console.log(">>> event.target.value", event.target.value);
+    setSearchField(event.target.value);
+    setCurrentPage(1); // Reset to first page on new search field
+    setSearchTerm(""); // Reset search term on new search field
+  };
 
   return (
     <>
       <header></header>
       <main className="container-fluid">
-        <input
-          type="search"
-          placeholder="Search by title"
-          value={searchTerm}
-          onChange={handleSearch}
-          style={{ marginTop: "10px" }}
-        />
-
-        {paginatedData.map((item, index) => {
-          return (
-            <div key={index}>
-              <h2
-                dangerouslySetInnerHTML={{
-                  __html: item["dc:title"],
-                }}
-              ></h2>
-              <p>
-                <strong>Author:</strong>{" "}
-                {item["author"].map((author, idx) => {
-                  if (typeof author === "string") {
-                    return author;
-                  }
-                  const authorName =
-                    (author["name"] || " ") +
-                    (author["family"] || "") +
-                    " " +
-                    (author["given"] || "");
-                  return (
-                    authorName + (idx !== item["author"].length - 1 ? "; " : "")
-                  );
-                })}
-              </p>
-              <p>
-                <strong>Affiliation:</strong>{" "}
-                {Array.isArray(item["affiliation"]) &&
-                  item["affiliation"]
-                    .map((affil) => {
-                      return [
-                        Array.isArray(affil["affilname"])
-                          ? affil["affilname"].join(", ")
-                          : affil["affilname"],
-                        affil["affiliation-city"],
-                        affil["affiliation-country"],
-                      ]
-                        .filter(Boolean)
-                        .join(", ");
-                    })
-                    .join("; ")}
-              </p>
-              {item["prism:doi"] && (
-                <p>
-                  <strong>DOI:</strong> {item["prism:doi"]}
-                </p>
-              )}
-              <p>
-                <strong>Journal:</strong> {item["prism:publicationName"]}
-              </p>
-              <p>
-                <strong>Publication Date:</strong>{" "}
-                {item["prism:coverDisplayDate"]} ({item["prism:coverDate"]})
-              </p>
-              <p>
-                <strong>Cited-by Count:</strong> {item["citedby-count"]}
-              </p>
-            </div>
-          );
-        })}
-
-        <div>
-          {Array.from({ length: totalPages }, (_, index) => {
-            if (totalPages > 10) {
-              if (
-                index === 0 ||
-                index === totalPages - 1 ||
-                (index >= currentPage - 2 && index <= currentPage + 2)
-              ) {
-                return (
-                  <button
-                    key={index}
-                    onClick={() => handlePageChange(index + 1)}
-                    disabled={currentPage === index + 1}
-                    style={{
-                      margin: "1px",
-                    }}
-                  >
-                    {index + 1}
-                  </button>
-                );
-              } else if (
-                index === currentPage - 3 ||
-                index === currentPage + 3
-              ) {
-                return <span key={index}>...</span>;
-              } else {
-                return null;
-              }
-            } else {
-              return (
-                <button
-                  key={index}
-                  onClick={() => handlePageChange(index + 1)}
-                  disabled={currentPage === index + 1}
-                  style={{
-                    margin: "1px",
-                  }}
-                >
-                  {index + 1}
-                </button>
-              );
-            }
-          })}
+        <div style={{ display: "flex", gap: "10px", marginTop: "10px" }}>
+          <input
+            type="search"
+            placeholder={`Search by ${SEARCH_FIELDS_MAPPING_REVERSE[
+              searchField
+            ].toLowerCase()}`}
+            value={searchTerm}
+            onChange={handleSearch}
+            style={{ flex: "0 0 80%" }}
+          />
+          <select
+            onChange={handleSearchFieldChange}
+            style={{ flex: "0 0 20%" }}
+          >
+            {Object.keys(SEARCH_FIELDS_MAPPING).map((field) => (
+              <option key={field} value={SEARCH_FIELDS_MAPPING[field]}>
+                {field}
+              </option>
+            ))}
+          </select>
         </div>
+
+        {renderData &&
+          renderData.map((item, index) => {
+            return (
+              <div key={index}>
+                <h2
+                  dangerouslySetInnerHTML={{
+                    __html: item["dc:title"],
+                  }}
+                ></h2>
+                <p>
+                  <strong>Author:</strong>{" "}
+                  {item["author"].map((author, idx) => {
+                    if (typeof author === "string") {
+                      return author;
+                    }
+                    const authorName =
+                      (author["name"] || " ") +
+                      (author["family"] || "") +
+                      " " +
+                      (author["given"] || "");
+                    return (
+                      authorName +
+                      (idx !== item["author"].length - 1 ? "; " : "")
+                    );
+                  })}
+                </p>
+                <p>
+                  <strong>Affiliation:</strong>{" "}
+                  {Array.isArray(item["affiliation"]) &&
+                    item["affiliation"]
+                      .map((affil) => {
+                        return [
+                          Array.isArray(affil["affilname"])
+                            ? affil["affilname"].join(", ")
+                            : affil["affilname"],
+                          affil["affiliation-city"],
+                          affil["affiliation-country"],
+                        ]
+                          .filter(Boolean)
+                          .join(", ");
+                      })
+                      .join("; ")}
+                </p>
+                {item["prism:doi"] && (
+                  <p>
+                    <strong>DOI:</strong> {item["prism:doi"]}
+                  </p>
+                )}
+                <p>
+                  <strong>Journal:</strong> {item["prism:publicationName"]}
+                </p>
+                <p>
+                  <strong>Publication Date:</strong>{" "}
+                  {item["prism:coverDisplayDate"]} ({item["prism:coverDate"]})
+                </p>
+                <p>
+                  <strong>Cited-by Count:</strong> {item["citedby-count"]}
+                </p>
+              </div>
+            );
+          })}
 
         <br />
         <div>
@@ -174,7 +175,6 @@ function App() {
             type="number"
             id="page-select"
             min="1"
-            max={totalPages}
             value={currentPage}
             onChange={(e) => handlePageChange(Number(e.target.value))}
             style={{ width: "150px" }}
